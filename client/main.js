7d2798053d1827aaeb124bc3e470b9cc76f77fd1
the-parthenon <ericinportland@gmail.com>
2022-04-15 11:20:31 -0700

    Four REST methods imur REST methods implemented.

--- client/main.js
+++ client/main.js
@@ -1,6 +1,85 @@
+const baseURL = `http://localhost:4000/api/`;
+
+const errCallback = (err) => console.log(err.response.data);
+const fortuneCallback = ({ data: fortuneArr }) => displayFortunes(fortuneArr);
+
 document.getElementById('complimentButton').onclick = function () {
-  axios.get('http://localhost:4000/api/compliment/').then(function (response) {
-    const data = response.data;
-    alert(data);
-  });
+  axios
+    .get(baseURL + `compliment`)
+    .then(function (response) {
+      const data = response.data;
+      alert(data);
+    })
+    .catch(errCallback);
+};
+
+document.getElementById('fortuneButton').onclick = () => {
+  axios
+    .get(baseURL + `fortune`)
+    .then(function (res) {
+      const data = res.data;
+      alert(data);
+    })
+    .catch(errCallback);
+};
+
+document.getElementById('addFortune').onsubmit = (evt) => {
+  evt.preventDefault();
+
+  let newFortune = document.querySelector('#newFortune');
+
+  let body = {
+    text: newFortune.value,
+  };
+
+  axios
+    .post(baseURL + 'fortunelist', body)
+    .then(fortuneCallback)
+    .catch(errCallback);
+
+  newFortune.value = '';
+};
+
+document.getElementById('allFortunes').onclick = () => {
+  axios
+    .get(baseURL + `fortunelist`)
+    .then(fortuneCallback)
+    .catch(errCallback);
+};
+
+const fortuneContainer = document.getElementById('fortuneContainer');
+
+const displayFortunes = (fortuneArr) => {
+  fortuneContainer.innerText = '';
+  for (let i = 0; i < fortuneArr.length; i++) {
+    const fortuneText = document.createElement('div');
+    fortuneText.innerHTML = `${fortuneArr[i].id}. ${fortuneArr[i].text}`;
+    fortuneContainer.appendChild(fortuneText);
+  }
+};
+
+document.getElementById('removeFortune').onsubmit = (evt) => {
+  evt.preventDefault();
+  let deadFortune = document.querySelector('#deadFortune').value;
+  //   console.log(deadFortune);
+  axios
+    .delete(baseURL + `fortunelist/${deadFortune}`)
+    .then(fortuneCallback)
+    .catch(errCallback);
+};
+
+document.getElementById('replaceFortune').onsubmit = (evt) => {
+  evt.preventDefault();
+
+  let newFortune = document.querySelector('#changeFortuneText');
+
+  let body = {
+    text: newFortune.value,
+  };
+
+  let param = document.querySelector('#changeFortuneID').value;
+  axios
+    .put(baseURL + `fortunelist/${param}`, body)
+    .then(fortuneCallback)
+    .catch(errCallback);
 };
--- server/index.js
+++ server/index.js
@@ -21,5 +21,41 @@ app.get('/api/compliment', (req, res) => {
   res.status(200).send(randomCompliment);
 });
 
+let fortuneArr = require('./db.json');
+
+app.get('/api/fortune', (req, res) => {
+  let randomIndex = Math.floor(Math.random() * fortuneArr.length);
+  let randomFortune = `${fortuneArr[randomIndex].text}`;
+
+  res.status(200).send(randomFortune);
+});
+
+app.get('/api/fortunelist', (req, res) => {
+  res.status(200).send(fortuneArr);
+});
+
+app.post('/api/fortunelist', (req, res) => {
+  const { text } = req.body;
+  let newFortune = {
+    id: fortuneArr.length + 1,
+    text,
+  };
+  fortuneArr.push(newFortune);
+  res.status(200).send(fortuneArr);
+});
+
+app.delete('/api/fortunelist/:id', (req, res) => {
+  let index = fortuneArr.findIndex((elem) => elem.id === +req.params.id);
+  fortuneArr.splice(index, 1);
+  res.status(200).send(fortuneArr);
+});
+
+app.put('/api/fortunelist/:id', (req, res) => {
+  let index = fortuneArr.findIndex((elem) => elem.id === +req.params.id);
+  const { text } = req.body;
+  fortuneArr[index].text = text;
+  res.status(200).send(fortuneArr);
+});
+
 const serverID = 4000;
 app.listen(serverID, () => console.log(`Server running on ${serverID}`));
